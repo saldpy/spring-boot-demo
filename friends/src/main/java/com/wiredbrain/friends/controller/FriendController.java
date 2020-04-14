e619c340c03c429c27f37a50d4ce455012527951
saldpy <53061673+saldpy@users.noreply.github.com>
2020-04-14 14:48:00 +0100

    initial commit

---
+++ friends/src/main/java/com/wiredbrain/friends/controller/FriendController.java
@@ -0,0 +1,54 @@
+package com.wiredbrain.friends.controller;
+
+import com.wiredbrain.friends.model.Friend;
+import com.wiredbrain.friends.service.FriendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class FriendController {
+
+    @Autowired
+    FriendService friendService;
+
+    @PostMapping("/friend")
+    Friend create(@RequestBody Friend friend){
+        return friendService.save(friend);
+    }
+
+    @GetMapping("/friend")
+    Iterable<Friend> read(){
+        return friendService.findAll();
+    }
+
+    @PutMapping("/friend")
+    Friend update(@RequestBody Friend friend){
+        return friendService.save(friend);
+    }
+
+    @DeleteMapping("/friend/{id}")
+    void delete(@PathVariable Integer id){
+        friendService.deleteById(id);
+    }
+
+    @GetMapping("/friend/{id}")
+    Optional<Friend> findById(@PathVariable Integer id){
+        return friendService.findById(id);
+    }
+
+    @GetMapping("/friend/search")
+    Iterable<Friend> findByQuery(
+            @RequestParam(value = "first", required = false) String firstName, @RequestParam (value = "last", required = false) String lastName)
+    {
+        if (firstName != null && lastName != null)
+            return friendService.findByFirstNameAndLastName(firstName, lastName);
+        else if (firstName != null)
+            return friendService.findByFirstName(firstName);
+        else if (lastName != null)
+            return friendService.findByLastName(lastName);
+        else
+            return friendService.findAll();
+    }
+}
